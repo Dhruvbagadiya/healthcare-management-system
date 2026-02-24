083758bc9e20c02efcbeada1474abcbfb16882b0
Dhruv Bagadiya <dhruvbagadiya@gmail.com>
2026-02-25 01:03:05 +0530

    remove code

--- frontend/src/lib/mobile-menu-toggle.tsx
+++ frontend/src/lib/mobile-menu-toggle.tsx
@@ -13,7 +13,6 @@ export function MobileMenuToggle() {
       aria-controls="mobile-menu"
       aria-expanded={isMobileMenuOpen}
     >
-      <span className="sr-only">Open main menu</span>
       {isMobileMenuOpen ? (
         <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
